6424719f9c83c11ca0cad7ea999fa5e0d39de050
francoisdefever <fdefever.work@gmail.com>
2016-02-25 11:53:16 +0100

    purge all db | migrate devise users | g devise:views

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160216095255) do
+ActiveRecord::Schema.define(version: 20160225092331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "brands", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "references"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -29,6 +36,12 @@ ActiveRecord::Schema.define(version: 20160216095255) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "phone"
+    t.boolean  "gender"
+    t.boolean  "subscribe"
+    t.string   "address"
+    t.string   "citycode"
+    t.string   "city"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
